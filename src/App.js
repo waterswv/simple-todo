241c5ea1b9763329f9ef932671b253edbb04a7ad
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-12-05 11:46:34 -0800

    created simple todo app in App.js with stateless functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
+import ToDoForm from './ToDoForm.js'
+import ToDoList from './ToDoList.js'
 import './App.css';
 
+// This is a stateless function ... another way of creating a class component
+const ToDoItem = ({props}) => (
+  <li>{props.text}</li>
+);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newToDO: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newToDo];
+    this.setState({todos, newToDo: ''});
+  }
   render() {
+    const {newToDo} = this.state;
+    const todos = this.state.todos.map((td, index) => (
+      <ToDoItem key={index} text={td}/>
+    ));
     return (
       <div className="App">
         <header>
           Bryan's To Do List
         </header>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="todo-input"
+            autoComplete="off"
+            type="text"
+            name="newToDo"
+            placeholder="What needs to be done"
+            value={newTodo}
+            onChange={(e) => this.setState({[e.target.name]: e.target.value})}
+          />
+        <button
+          type="button"
+          className="save-button"
+          >
+          Save
+        </button>
+        </form>
+      <div className="todo-content">
+        <ol>
+          {todos}
+        </ol>
+      </div>
       </div>
     );
   }
